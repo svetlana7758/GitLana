186a18ac63df19058958511f7a497293caa23b71
Svetlana <svetlana@svetlana-VirtualBox.(none)>
2012-12-11 12:25:55 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lanaapp::Application.config.secret_token = '336f3e79bcb3912bf2d20efc14613f76d9f390ebc6cfe233fa80890af7966168156ca5cfa70d601608bb43c0e51a2632e5a966ac3da94deccb49b9555b6d8413'
